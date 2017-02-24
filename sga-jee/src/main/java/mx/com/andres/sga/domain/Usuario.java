a930c83fcc12251c7ec5fcf09244734907fabea7
andresrayado86 <andres_aguilar_86@hotmail.com>
2017-02-24 12:01:06 -0600

    Se agrega la configuracion del proyecto sga-jee para la conversion a proyecto JPA, se crea la clase de entidad usuario.

---
+++ sga-jee/src/main/java/mx/com/andres/sga/domain/Usuario.java
@@ -0,0 +1,65 @@
+package mx.com.andres.sga.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the usuario database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Usuario.findAll", query="SELECT u FROM Usuario u")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_usuario")
+	private int idUsuario;
+
+	private String password;
+
+	private String username;
+
+	//bi-directional many-to-one association to Persona
+	@ManyToOne(cascade={CascadeType.ALL})
+	@JoinColumn(name="id_persona")
+	private Persona persona;
+
+	public Usuario() {
+	}
+
+	public int getIdUsuario() {
+		return this.idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Persona getPersona() {
+		return this.persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+
+}
